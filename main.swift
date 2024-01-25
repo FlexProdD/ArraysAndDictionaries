1f58c2ce0740f013b90e1f87d7514fd9ee7d898c
FlexProdD <112783610+FlexProdD@users.noreply.github.com>
2024-01-25 19:02:41 +0300

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  ios_giris
+//
+//  Created by Flex Prod on 25.01.2024.
+//
+
+import Foundation
+
+var dizi:[Int]=[08,53,37]
+var sozluk:Dictionary<String,Int> = ["Kastamonu":37, "Rize":53, "Artvin":08]
+print("Diziler \n")
+for diziler in dizi{
+    print(diziler)
+}
+print("Sözlükler \n")
+for sozlukler in sozluk{
+    print(sozlukler)
+}
+
